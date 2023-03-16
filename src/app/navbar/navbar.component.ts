62134fb64b81cf63dfe6028cf1cb4c2126d6ba07
hassanm <s1136140@student.hsleiden.nl>
2023-03-16 11:36:28 +0100

    wop: counter total price

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CartService} from "../service/cart.service";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  constructor(private cartService: CartService) {
+  }
+
+  ngOnInit(): void {
+    this.cartService.cart.subscribe(
+      {
+
+      }
+    )
+  }
 
 }
